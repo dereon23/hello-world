006909a545c55ac718b0a26adc84f583f1cbd71f
Diego <dleonrz23@gmail.com>
2023-10-01 20:28:35 +0200

    commit 2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/home/user-info/user-info.component';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  users = [
-    { nombre: 'Juan', apellidos: 'Pérez Ramírez', edad: 18 },
-    { nombre: 'María', apellidos: 'León González', edad: 16 },
-    { nombre: 'Luis', apellidos: 'Sánchez Rodríguez', edad: 23 },
-    { nombre: 'Ana', apellidos: 'Fernandez Martínez', edad: 21 },
+
+  _users = [
+    { name: 'Juan', surname: 'Pérez Ramírez', age: 18 },
+    { name: 'María', surname: 'León González', age: 16 },
+    { name: 'Luis', surname: 'Sánchez Rodríguez', age: 23 },
+    { name: 'Ana', surname: 'Fernandez Martínez', age: 21 },
   ];
+
+  next(users: User[]): void {
+    this._users = users;
+  }
+
+  ngOnInit(): void {
+  
+    var users:User[] = [
+      {id: 1, name:"Juan A.", surname:"garcía gómez", age:46, fav:true},
+      {id: 2, name:"Alejandro", surname:"garcía gómez", age:45, fav:true},
+      {id: 3, name:"juan", surname:"garcía Valencia", age:4, fav:false},
+      {id: 4, name:"María del Mar", surname:"Valencia Valencia", age:46, fav:true},
+      {id: 5, name:"Lydia", surname:"garcía Robles", age:11, fav:false}
+    ];
+    this.next(users);
+  }
 }
--- src/app/home/user-info/user-info.component.ts
+++ src/app/home/user-info/user-info.component.ts
@@ -1,18 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+
+
+export interface User {
+  id:number,
+  name:string,
+  surname:string,
+  age:number,
+  fav:boolean
+}
+
+
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss'],
 })
-export class UserInfoComponent implements OnInit {
-  @Input() nombre: string = '';
-  @Input() apellidos: string = '';
-  @Input() edad: number = 0;
+
+
+export class UserInfoComponent implements OnInit, User {
+  @Input() id:number = 0;
+  @Input() name: string = '';
+  @Input() surname: string = '';
+  @Input() age: number = 0;
+  @Input() fav:boolean = false;
 
   constructor() {}
 
   ngOnInit() {}
 }
+
+
+/*onFavClick(event:any){
+  this.onFavClicked.emit({
+    fav:!(this.user?.fav??false) //devolvemos el estado contrario al que tenemos
+  });
+  event.stopPropagation();
+}*/
+
+export interface UserInfoFavClicked{
+  fav:boolean | undefined;
+ }
+ 
+
+//@Output() onFavClicked:EventEmitter<UserInfoFavClicked> = new EventEmitter<UserInfoFavClicked>();
 
 
